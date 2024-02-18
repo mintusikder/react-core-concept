c851e6e34df6513eee5ea78483949629e32f7834
mintusikder <mintusikder15@gmail.com>
2024-02-18 23:13:46 +0600

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Person from './assets/components/Person'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Person></Person>
     </>
   )
 }
---
+++ src/assets/components/Person.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <h2>Mintu Sikder</h2>
+        </div>
+    );
+};
+
+export default Person;
